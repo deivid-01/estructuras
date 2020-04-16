b7071d97f08164ba604d1ea80aa798c5cb528bf2
JulianaCadavid <juliana.cadavidr@udea.edu.co>
2020-04-15 21:09:16 -0500

    Login y registro done

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './componentes/home-page/home-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import {RegisterPageComponent} from './componentes/register-page/register-page.component';
+
+
+const routes: Routes = [
+  {path:'', component: HomePageComponent},
+  {path:'login', component: LoginPageComponent},
+  {path:'register', component: RegisterPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
