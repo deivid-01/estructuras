7ba7fb3e7cca8b148879469d145f7e6989c95e44
Mateo Alvarez Madrigal <mateoalvmad@gmail.com>
2020-05-03 16:22:10 -0500

    narval

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RegisterPageComponent} from './componentes/register-page/register-page.component';
-
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { UserPageComponent } from './componentes/user-page/user-page.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
     NavbarComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
+    HomePageComponent,
+    UserPageComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/componentes/home-page/home-page.component.ts
+++ frontend/src/app/componentes/home-page/home-page.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  usName: string = "Nombre Usuario";
+
   constructor() { }
 
   ngOnInit(): void {
